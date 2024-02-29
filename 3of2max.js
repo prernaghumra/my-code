265943ff1037c90e5da1fa77883dde1fcb347566
prernaghumra <161703602+prernaghumra@users.noreply.github.com>
2024-02-29 15:09:00 +0530

    Add files via upload

---
+++ 3of2max.js
@@ -0,0 +1,35 @@
+const input = require("readline-sync");
+let a = input.questionInt("enter first number");
+let b = input.questionInt("enter second number");
+let c = input.questionInt("enter third number");
+if(a>b){
+    l=a;
+    s=b;
+}
+else
+{
+    l=b;
+    s=a;
+}
+if(l>c){
+    l1=l;
+    s1=c;
+}
+else{
+    l1=c;
+    s1=l;
+}
+if(s>s1){
+    l2=s;
+    s2=s1;
+}
+else{
+    l2=s1;
+    s2=s;
+}
+if(l2>s2){
+    console.log("second max",l2);
+}
+else{
+    console.log("second max",s2);
+}
