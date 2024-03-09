a27200fa7c58d8d240a60c1b733fc77eebf24223
prernaghumra <161703602+prernaghumra@users.noreply.github.com>
2024-03-09 11:28:39 +0530

    date of birth
    date of birth

---
+++ v2.js
@@ -0,0 +1,7 @@
+const input= require("readline-sync");
+let d= input.questionInt("enter the day");
+let m= input.questionInt("enter the month");
+let y= input.questionInt("enter the year");
+console.log(d);
+console.log(m);
+console.log(y);
