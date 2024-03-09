f12d6bd878c04b9b536a18e3e3bf7ee2c011cdb6
prernaghumra <161703602+prernaghumra@users.noreply.github.com>
2024-03-09 11:05:02 +0530

    HLcm
    HLcm

---
+++ HLcm.js
@@ -0,0 +1,31 @@
+
+const input = require("readline-sync")
+let a = input.questionInt("enter the number:")
+let b = input.questionInt("enter the number:")
+if(a>b)
+{
+    m = a;
+    n = b;
+}
+else{
+    m = b;
+    n = a;
+}
+  while(a==b)
+  elseif(a>b)
+  a = a-b;
+  b = b-a;
+
+  hcf = a;
+{
+    console.log(hcf)
+}
+
+{
+    lcm = m*n/hcf
+}
+
+{
+    console.log(lcm)
+
+}
