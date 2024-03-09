309993685f3b81627818a4ac494a439901d71761
prernaghumra <161703602+prernaghumra@users.noreply.github.com>
2024-03-09 10:22:46 +0530

    frequency
    frequency

---
+++ array7.js
@@ -0,0 +1,36 @@
+const input= require("readline-sync");
+let n = input.questionInt("enter the size of array:");
+let a = [];
+let i = 0;
+while(i<n)
+{
+    a[i] = input.questionInt("num:");
+    i++;
+}
+i=0;
+while(i<n)
+{
+    let j =i+1;
+    let p=1;
+    while (j<n){
+        if (a[i]==a[j])
+        {
+          k = j;
+          p++;
+          while(k<n-1)
+          {
+            a[k]=a[k+1];
+            k=k+1;
+          }
+          n=n-1
+        }
+        else
+        {
+            p==p;
+            j++;
+        }
+    }
+    console.log(a[i]+"-"+p);
+    i++;
+    j=i+1;
+}
