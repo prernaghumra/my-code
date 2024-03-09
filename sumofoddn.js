555309cd1ec82955d65d690df7d56730c96ecaa5
prernaghumra <161703602+prernaghumra@users.noreply.github.com>
2024-03-09 11:11:06 +0530

    sum of odd number
    sum of odd number

---
+++ sumofoddn.js
@@ -0,0 +1,13 @@
+const input= require("readline-sync");
+let n = input.questionInt("enter the number:");
+let i = 1;
+let s = 0;
+while(i<n)
+{
+    if(i%2!=0)
+    {s=s+i}
+    i++;
+}
+{
+    console.log(s);
+}
