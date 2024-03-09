c406acaaec2bbc47adc63e8ec7531083bca9b7d1
prernaghumra <161703602+prernaghumra@users.noreply.github.com>
2024-03-09 11:25:38 +0530

    six basic operations
    six basic operations

---
+++ v5.js
@@ -0,0 +1,13 @@
+const input= require("readline-sync");
+let a = input.questionInt("enter the a number");
+let b = input.questionInt("enter the b number");
+ad =a+b;
+s= a-b;
+m= a*b;
+d = a/b;
+id= Math.floor(a/b);
+mo=a%b;{
+    console.log(ad,s,m,d,id,mo);
+}
+
+
