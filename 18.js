a0936f13c0ebabb72784a8c9a3de76762c246fa2
prernaghumra <161703602+prernaghumra@users.noreply.github.com>
2024-03-15 12:12:19 +0530

    Merge them into a single sorted array
     Merge them into a single sorted array

---
+++ 18.js
@@ -0,0 +1,39 @@
+const input = require("readline-sync")
+let m = input.questionInt("enter the number: ");
+let n = input.questionInt("enter the number: ");
+ let a=[];
+ let b =[];
+let s=m+n
+ let c =[];
+let i = 0;
+while(i<m)
+{
+    a[i]=input.questionInt("enter check number: ")
+    i++;
+}
+let j=0;
+while(j<n)
+{
+    b[j]=input.questionInt("enter number: ")
+    j++;
+}
+let f=0
+while(f<s)
+{
+    c[f]=0
+    f++;
+}
+let d=0;
+while(d<m)
+{
+    c[d]=a[d]
+    d++
+}
+let k=0;
+while(d<s)
+{
+    c[d]=b[k]
+    k=k+1;
+    d++;
+}
+ console.log(c);
